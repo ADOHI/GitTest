e4d9fd62ea8bbd2b2c2ff8e68fa93e5e38f93b8b
adohi <adohi0824@gmail.com>
2023-10-17 20:07:02 +0900

    Fix TestScript

--- Assets/Scripts/GitTestScript.cs
+++ Assets/Scripts/GitTestScript.cs
@@ -7,7 +7,7 @@ public class GitTestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("firstProject");
     }
 
     // Update is called once per frame
